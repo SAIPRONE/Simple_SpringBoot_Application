8d2934bd31863f774ff585ac8f243cdb55f6bfca
SAIPRONE <fadi.helal@pm.me>
2023-03-11 14:38:31 +0200

    Added Methods: Update(), Delete()

--- saibrone/src/main/java/saibrones/ProductController.java
+++ saibrone/src/main/java/saibrones/ProductController.java
@@ -1,9 +1,12 @@
 package saibrones;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -37,5 +40,21 @@ public class ProductController {
 	@PostMapping("/products")
 	public void add(@RequestBody Product product) {
 		service.save(product);
+	}
+	@PutMapping("/products/{id}")
+	public ResponseEntity<?> update(@RequestBody Product product,
+			@PathVariable Integer id) {
+		try {
+			Product existProduct = service.get(id);
+			service.save(product);
+			
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch (NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	@DeleteMapping("/products{id}")
+	public void delete(@RequestParam Integer id) {
+		service.delete(id);
 	}
 }
